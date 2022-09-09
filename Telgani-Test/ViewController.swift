9b5e9c9651b1288e84c9440816e167b6befd5d05
Mostafa Gadallah <mostafagadallah93@gmail.com>
2022-09-09 23:42:41 +0300

    Initial Commit

---
+++ Telgani-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telgani-Test
+//
+//  Created by Moustafa Gadallah on 13/02/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Telgani-TestTests/Telgani_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  Telgani_TestTests.swift
+//  Telgani-TestTests
+//
+//  Created by Moustafa Gadallah on 13/02/1444 AH.
+//
+
+import XCTest
+@testable import Telgani_Test
+
+class Telgani_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
